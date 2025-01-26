0b80b7050efc8ae370e80b9a1e239eacb36ee832
rafikul <babu@linux.local>
2025-01-26 20:48:36 +0530

    foodorder admin completed

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "../src/components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Order from "./pages/Orders/Order";
+import Verify from "../../client/src/pages/verify/Verify";
+
+const App = () => {
+  const url = "https://foodbackend-tx3z.onrender.com";
+
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/order" element={<Order url={url} />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
